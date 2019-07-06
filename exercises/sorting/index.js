853ed637c184f4d6e2ffb2fd24c19877f52905dc
lissett-fundora <lissett.fundora@officedepot.com>
2019-07-06 17:22:14 -0400

    Update merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -10,28 +10,29 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
-  if(arr.length === 1){
-    return arr;
-  }
+    if (arr.length === 1) {
+        return arr;
+    }
 
-  const center  = Math.floor(arr.length / 2);
-  const left = arr.slice(0, center);
-  const right = arr.slice(center);
+    const center = Math.floor(arr.length / 2);
+    const left = arr.slice(0, center);
+    const right = arr.slice(center);
 
-  return merge(mergeSort(left), mergeSort(right) );
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-  const results = [];
-  while(left.length && right.length){
-    if(left.length > right.length) {
-      results.push(righ.shift);
-    } else {
-      results.push(left.shift);
-    }
-  }
+    const results = [];
 
-  return [...results, ...left, ...right];
+    while (left.length && right.length) {
+        if (left[0] < right[0]) {
+            results.push(left.shift());
+        } else {
+            results.push(right.shift());
+        }
+    }
+
+    return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
